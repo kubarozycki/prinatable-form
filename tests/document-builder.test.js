4dda44515f3eb285b8606743d948fe85c7408b8b
Developer <developer@MacBook-Air.local>
2023-05-03 20:48:25 +0200

    handlebars + pupeeter to generate pdf

--- tests/document-builder.test.js
+++ tests/document-builder.test.js
@@ -1,6 +1,7 @@
 const fs = require("fs/promises");
 const path = require("path");
 const Handlebars = require("handlebars");
+const puppeteer = require("puppeteer");
 
 describe("document-builder tests", () => {
   it("generate document based on payload", async () => {
@@ -14,8 +15,25 @@ describe("document-builder tests", () => {
     const template = Handlebars.compile(templateFileContent);
     expect(template).toBeTruthy();
 
-    const htmlDoc = template({ name: "Adam Małysz" });
+    const htmlDoc = template({ name: "Adam", surname: "Małysz" });
     console.log(htmlDoc);
     expect(htmlDoc).toBeTruthy();
+
+    const browser = await puppeteer.launch({
+      args: ["--disable-dev-shm-usage", "'--shm-size=3gb'"],
+    });
+    const page = await browser.newPage();
+
+    await page.setContent(htmlDoc);
+
+    await page.pdf({
+      path: path.join(__dirname, "../dist", "generated-doc.pdf"),
+      margin: { top: "100px", right: "50px", bottom: "100px", left: "50px" },
+      printBackground: true,
+      format: "A4",
+    });
+
+    await page.close();
+    await browser.close();
   });
 });
